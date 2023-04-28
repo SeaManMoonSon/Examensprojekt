2af3b9f2252b2c9d8cb160c553e9841e972220c7
vselenwall <veronica@selenwall.se>
2023-04-28 13:37:53 +0200

    Added pages

---
+++ GlimCrub/client/src/pages/users/Categories.js
@@ -0,0 +1,11 @@
+
+const Categories = () => {
+    return (
+        <div className="categories">
+                <h2>Welcome, username!</h2>
+                <h3>Component for menu</h3>
+        </div>
+    )
+}
+
+export default Categories
