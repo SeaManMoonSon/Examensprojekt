87c3e59924fc7bd38a2dae990e16331ea6d8b78d
vselenwall <veronica@selenwall.se>
2023-05-03 09:54:16 +0200

    wip

--- GlimCrub/client/src/pages/users/UserStart.jsx
+++ GlimCrub/client/src/pages/users/UserStart.jsx
@@ -3,44 +3,44 @@ import { useEffect, useState } from "react"
 
 
 const UserStart = () => {
-    const [users, setUsers] = useState(null)
-
-    useEffect(() =>  {
-        const fetchUser = async () => {
-            const response = await fetch('/api/users')
-            const json = await response.json()
-
-            if (response.ok) {
-                setUsers(json)
-            }
-        }
-
-        fetchUser()
-    }, [])
-
-
-    // const [products, setProducts] = useState(null)
+    // const [users, setUsers] = useState(null)
 
     // useEffect(() =>  {
-    //     const fetchMenu = async () => {
-    //         const response = await fetch('/api/products')
+    //     const fetchUser = async () => {
+    //         const response = await fetch('/api/users')
     //         const json = await response.json()
 
     //         if (response.ok) {
-    //             setProducts(json)
+    //             setUsers(json)
     //         }
     //     }
 
-    //     fetchMenu()
+    //     fetchUser()
     // }, [])
+
+
+    const [products, setProducts] = useState(null)
+
+    useEffect(() =>  {
+        const fetchMenu = async () => {
+            const response = await fetch('/api/products')
+            const json = await response.json()
+
+            if (response.ok) {
+                setProducts(json)
+            }
+        }
+
+        fetchMenu()
+    }, [])
 
     return (
         <div className="start">
                 <h2>Glimcrub</h2>
                 <h3>Component for login</h3>
             <div className="users">
-                {users && users.map((user) => (
-                    <p key={user._id}>{user.name}</p>
+                {products && products.map((product) => (
+                    <p key={product._id}>{product.menu.breakfast}</p>
                 ))}
             </div>
         </div>
