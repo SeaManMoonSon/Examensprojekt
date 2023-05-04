5bccc8f9b01298e7f65c80eba8d073c9a14d1065
SeaManMoonSon <simonmansson94@hotmail.com>
2023-05-04 11:51:11 +0200

    fetch

--- GlimCrub/client/src/pages/users/UserStart.jsx
+++ GlimCrub/client/src/pages/users/UserStart.jsx
@@ -1,58 +1,55 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 // components
-import UserLogin from '../../components/users/UserLogin'
-
-
+import UserLogin from "../../components/users/UserLogin";
 
 const UserStart = () => {
-    // const [users, setUsers] = useState(null)
+  // const [users, setUsers] = useState(null)
 
-    // useEffect(() =>  {
-    //     const fetchUser = async () => {
-    //         const response = await fetch('/api/users')
-    //         const json = await response.json()
+  // useEffect(() =>  {
+  //     const fetchUser = async () => {
+  //         const response = await fetch('/api/users')
+  //         const json = await response.json()
 
-    //         if (response.ok) {
-    //             setUsers(json)
-    //         }
-    //     }
+  //         if (response.ok) {
+  //             setUsers(json)
+  //         }
+  //     }
 
-    //     fetchUser()
-    // }, [])
+  //     fetchUser()
+  // }, [])
 
+  const [products, setProducts] = useState(null);
 
-    const [products, setProducts] = useState(null)
+  useEffect(() => {
+    const fetchMenu = async () => {
+      const response = await fetch("/api/products");
+      const json = await response.json();
 
-    useEffect(() =>  {
-        const fetchMenu = async () => {
-            const response = await fetch('/api/products')
-            const json = await response.json()
+      if (response.ok) {
+        setProducts(json);
+      }
+    };
 
-            if (response.ok) {
-                setProducts(json)
-            }
-        }
+    fetchMenu();
+  }, []);
 
-        fetchMenu()
-    }, [])
+  return (
+    <div className="start">
+      <h2>Glimcrub</h2>
+      <h3>Component for login</h3>
+      <div className="users">
+        {products &&
+          products.map(
+            (product) =>
+              product.menu &&
+              product.menu.snack.map((item) => (
+                <p key={item._id}>{item.name}</p>
+              ))
+          )}
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div className="start">
-                <h2>Glimcrub</h2>
-                <h3>Component for login</h3>
-
-            <div className="login">
-                <UserLogin />
-            </div>
-
-            <div className="users">
-                {products && products.map((product) => (
-                    <p key={product._id}>{product.menu.breakfast}</p>
-                ))}
-            </div>
-        </div>
-    )
-}
-
-export default UserStart
+export default UserStart;
