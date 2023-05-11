49595ece145dfa6c8112941e614b7bfc1040692c
vselenwall <veronica@selenwall.se>
2023-05-11 10:59:28 +0200

    logout function

--- GlimCrub/client/src/hooks/useLogout.jsx
+++ GlimCrub/client/src/hooks/useLogout.jsx
@@ -1,6 +1,12 @@
+import { useAuthContext } from './userAuthContext'
+
 export const useLogout = () => {
 
     const logout = () => {
-        
+        localStorage.remo('user')
+
+        dispatchEvent({type: 'LOGOUT'})
     }
+
+    return {logout}
 }
