cb46d89623a8b70a6525ad7db00617b73ce4a8c6
SeaManMoonSon <simonmansson94@hotmail.com>
2023-05-16 14:22:41 +0200

    Linode

--- GlimGrub/client/src/proxyURL.js
+++ GlimGrub/client/src/proxyURL.js
@@ -1,7 +1,7 @@
 // Linode server
-// const URL = "http://143.42.27.241:3000";
+const URL = "http://143.42.27.241:3000";
 
 // Localhost
-const URL = "http://localhost:3000";
+// const URL = "http://localhost:3000";
 
 export default URL;
