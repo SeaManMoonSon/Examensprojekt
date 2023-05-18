ee7e18302e60da68744e53f8606cb62f9e1b1f7e
vselenwall <veronica@selenwall.se>
2023-05-18 11:35:47 +0200

    Set up list of users in admin

--- GlimGrub/client/src/pages/admin/Users.jsx
+++ GlimGrub/client/src/pages/admin/Users.jsx
@@ -1,13 +1,33 @@
 import React from 'react';
+import { useEffect, useState } from "react"
+import URL from "../../proxyURL.js";
 
 // components
 import Logout from "../../components/misc/Logout";
 import AdminNavbar from "../../components/admin/AdminNavbar";
+import { useAuthContext, userAuthContext } from "../../hooks/userAuthContext";
 
 // styles
 import '../../sass/style.scss'
 
 const Users = () => {
+    // const { user } = useAuthContext();
+
+    const [users, setUsers] = useState(null)
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            const response = await fetch(`${URL}/api/users`);
+            const json = await response.json()
+
+            if (response.ok) {
+                setUsers(json)
+            }
+        }
+
+        fetchUsers()
+    }, [])
+
     return (
         <div className="admin__container">
             <AdminNavbar />
@@ -19,10 +39,24 @@ const Users = () => {
                 <button>Deltagare</button>
             </div>
             <div className="admin__show-users">
-                <p>Här kommer personal/deltagare listas via en component</p>
                 <ul>
-                    <div className="admin__show-users_list"><p>Simon Månsson</p><div className="admin__show-users_list-balance"><button>Redigera saldo</button></div></div>
-                    <div className="admin__show-users_list"><p>Veronica Selenwall</p><div className="admin__show-users_list-balance"><button>Redigera saldo</button></div></div>
+                    {/* {users.users.name} */}
+
+                    {users && (
+                        <>
+                            {users.map((user) => {
+                                return <div className="admin__show-users_list" key={user._id}>
+                                    <p>{user.name}</p>
+                                    <p>{user.balance}</p>
+                                    <div className="admin__show-users_list-balance"><button>Redigera saldo</button></div>
+                                </div>;
+                            })}
+                        </>
+                    )}
+
+
+                    {/* <div className="admin__show-users_list"><p>Simon Månsson</p><div className="admin__show-users_list-balance"><button>Redigera saldo</button></div></div> */}
+                    {/* <div className="admin__show-users_list"><p>Veronica Selenwall</p><div className="admin__show-users_list-balance"><button>Redigera saldo</button></div></div> */}
                 </ul>
             </div>
         </div>
