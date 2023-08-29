6b180369fa21fd3139320c55d6918a8ac08756ed
SeaManMoonSon <simonmansson94@hotmail.com>
2023-08-29 09:34:51 +0200

    undo purchase startup

--- GlimGrub/client/src/proxyURL.js
+++ GlimGrub/client/src/proxyURL.js
@@ -2,9 +2,9 @@
 // const URL = "http://139.144.179.83:4000";
 
 // Linode server
-const URL = "http://172.104.241.113:4000";
+// const URL = "http://172.104.241.113:4000";
 
 // Localhost
-// const URL = "http://localhost:4000";
+const URL = "http://localhost:4000";
 
 export default URL;
